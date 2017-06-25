2bccaec914e12dfd40ec437c86c9c0e70084a152
Jinxiaohai <xiaohaiJin@outlook.com>
2017-06-25 20:38:41 +0800

    first

---
+++ scipy/chapter3/help/help.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+"""document of the module"""
+
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy import optimize
+
+
+np.info(optimize.fmin)
